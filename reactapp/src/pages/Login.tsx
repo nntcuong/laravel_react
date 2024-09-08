e39c3aac88f268987047e2c26cf1082d51b65000
nntcuong <thecuongyt3htk@gmail.com>
2024-09-08 07:43:30 +0700

    'fdf'

--- reactapp/src/pages/Login.tsx
+++ reactapp/src/pages/Login.tsx
@@ -2,14 +2,12 @@ const Login = () => {
     return (
 
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
-                <h1 className="text-lg font-bold mb-6 text-center">
-                    Login
-                </h1>
+            <div className="bg-white p-5 rounded shadow-lg w-full max-w-md">
+              
                 <form action="">
                     <div className="mb-4">
 
-                        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+                        <label htmlFor="email" className="block text-gray-700 text-sm font-medium mb-2">Email:</label>
                         <input
                             type="text"
                             id="email"
@@ -19,7 +17,7 @@ const Login = () => {
                     </div>
                     <div className="mb-4">
 
-                        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password:</label>
+                        <label htmlFor="password" className="block text-gray-700 text-sm font-medium mb-2">Password:</label>
                         <input
                             type="password"
                             id="password"
@@ -27,17 +25,18 @@ const Login = () => {
                             className="w-full p3 border border-gray-700 rounded-xl focus:outline-none focus:ring focus-blue-200 h-10"
                         />
                     </div>
-                    <div className="mb-6">
+                    <div className="mb-2">
                         <button
                         type="submit"
-                        className="w-full bg-blue-500 text-white hover:bg-blue-700 py-3 rounded-md"
-                        
-
+                        className="w-full bg-blue-500 text-white hover:bg-blue-700 py-1.5 shadow-button rounded-md"
                         >
                             Login
                         </button>
 
                     </div>
+                    <p>
+                        <a href="/" className="text-blue-700">Forgot password</a>
+                    </p>
                 </form>
 
 
